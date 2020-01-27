be1f655ea4943f3af87c43f6294e54e107101dd2
ade yoga <57425861+adeyoga28@users.noreply.github.com>
2020-01-27 12:26:48 +0700

    Add files via upload

---
+++ javaArray/src/kelasku.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author windbit10
+ */
+public class kelasku {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      // TODO code application logic here
+    String[][]keluarga={{"ayah","Paidi"},{"ibu","Nawang"},{"anak1","Ade"}};
+            for (int x=0;x<keluarga.length;x++){
+                System.out.println("Status:"+keluarga[x][0]);
+                System.out.println("Nama:"+keluarga[x][1]);
+                System.out.println("-----------------------------");
+            }
+    }
+    
+}
